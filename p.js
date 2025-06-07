475b036395ee756e667d3c7db47ebd6c65302f3a
Asiphe123! <andiswadlomo88@gmail.com>
2025-06-07 21:58:45 +0200

    added line 5 and 6

--- p.js
+++ p.js
@@ -2,3 +2,5 @@ console.log("Hello world");
 console.log("enter your name");
 console.log("enter your age");
 console.log ("are you a main or female");
+console.log("enter your adress");
+console.log("enter your password");
